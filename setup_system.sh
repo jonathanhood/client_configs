62af583f19edc63d3e5f097f52fb273b54664eb4
Jonathan Hood <jonathan.hood@adtran.com>
2014-11-05 09:24:41 -0600

    Add screenrc

--- setup_system.sh
+++ setup_system.sh
@@ -6,6 +6,7 @@ ln -s $CWD/vimrc $HOME/.vimrc
 ln -s $CWD/vimfiles $HOME/.vim
 ln -s $CWD/gitconfig $HOME/.gitconfig
 ln -s $CWD/pam_environment $HOME/.pam_environment
+ln -s $CWD/screenrc $HOME/.screenrc
 
 touch ~/.bashrc
 
