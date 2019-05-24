c3f0b1adb52bc5cc60355a5ae43c35e95f1494e2
Jonathan Hood <jhood06@gmail.com>
2019-05-23 20:23:21 -0500

    Remove p4 config. Change email

--- setup_system.sh
+++ setup_system.sh
@@ -5,7 +5,6 @@ CWD=$(pwd)
 ln -s $CWD/vimrc $HOME/.vimrc
 ln -s $CWD/vimfiles $HOME/.vim
 ln -s $CWD/gitconfig $HOME/.gitconfig
-ln -s $CWD/pam_environment $HOME/.pam_environment
 ln -s $CWD/screenrc $HOME/.screenrc
 ln -s $CWD/tmux.conf $HOME/.tmux.conf
 mkdir -p $HOME/.tmux/plugins
